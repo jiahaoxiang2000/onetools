6fadd3843d06ac198f21b5038fa3ad3ff10cfbc3
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-05-02 22:45:22 +0800

    add the element plus

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,37 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useCounterStore } from '../stores/counter'
-let counterStore = useCounterStore()
-let show = ref(true)
-let checked = ref(false)
-const selected = ref('A')
-
-const options = ref([
-  { text: 'One', value: 'A' },
-  { text: 'Two', value: 'B' },
-  { text: 'Three', value: 'C' }
-])
 </script>
 
 <template>
-  <select v-model="selected">
-    <option v-for="op in options" :value="op.value" :key="op.value">
-      {{ op.text }}
-    </option>
-  </select>
-
-  <div>Selected: {{ selected }}</div>
-  <a v-if="show">{{ counterStore.count }} {{ counterStore.doubleCount }}</a>
-  <button @click="counterStore.increment">Increment</button>
-  <input type="checkbox" id="checkbox" v-model="checked" />
-  <label for="checkbox">{{ checked }}</label>
-  <button
-    @click="
-      () => {
-        show = !show
-      }
-    "
-  >
-    show
-  </button>
+  <div></div>
 </template>
