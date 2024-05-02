a6674300213f51fc1c28e793d3047c545a0c4b84
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-05-02 16:18:00 +0800

    v-if to show

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useCounterStore } from '../stores/counter'
 let counterStore = useCounterStore()
+let show = ref(true)
 </script>
 
 <template>
-  <a>{{ counterStore.count }} {{ counterStore.doubleCount }}</a>
+  <a v-if="show">{{ counterStore.count }} {{ counterStore.doubleCount }}</a>
   <button @click="counterStore.increment">Increment</button>
+  <button
+    @click="
+      () => {
+        show = !show
+      }
+    "
+  >
+    show
+  </button>
 </template>
