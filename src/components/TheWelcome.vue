ac9db8bf5b7f1e44db27721c11ac130e9564196a
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-05-02 16:55:34 +0800

    model: for the data input

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,11 +3,14 @@ import { ref } from 'vue'
 import { useCounterStore } from '../stores/counter'
 let counterStore = useCounterStore()
 let show = ref(true)
+let checked = ref(false)
 </script>
 
 <template>
   <a v-if="show">{{ counterStore.count }} {{ counterStore.doubleCount }}</a>
   <button @click="counterStore.increment">Increment</button>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
   <button
     @click="
       () => {
