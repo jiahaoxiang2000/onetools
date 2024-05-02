d22b3731a05d9450eb2248741ca19752d5a0f64b
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-05-02 17:25:35 +0800

    model: multi object

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,9 +4,23 @@ import { useCounterStore } from '../stores/counter'
 let counterStore = useCounterStore()
 let show = ref(true)
 let checked = ref(false)
+const selected = ref('A')
+
+const options = ref([
+  { text: 'One', value: 'A' },
+  { text: 'Two', value: 'B' },
+  { text: 'Three', value: 'C' }
+])
 </script>
 
 <template>
+  <select v-model="selected">
+    <option v-for="op in options" :value="op.value" :key="op.value">
+      {{ op.text }}
+    </option>
+  </select>
+
+  <div>Selected: {{ selected }}</div>
   <a v-if="show">{{ counterStore.count }} {{ counterStore.doubleCount }}</a>
   <button @click="counterStore.increment">Increment</button>
   <input type="checkbox" id="checkbox" v-model="checked" />
